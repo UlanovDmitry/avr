f730b65578f22b81396889e7b2c9c8aabcd6b398
Ulanov_Dmitry <ulanov.ktn@gmail.com>
2019-02-22 09:32:36 +0700

    uart c

---
+++ uart0c/uart0c.c
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+
+#define FOSC 16000000 // Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
+void USART_Init( unsigned int ubrr)
+{
+/*Set baud rate */
+UBRR0H = (unsigned char)(ubrr>>8);
+UBRR0L = (unsigned char)ubrr;
+/*Enable receiver and transmitter */
+UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+/* Set frame format: 8data, 2stop bit */
+UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+int main( void )
+{
+
+USART_Init(MYUBRR);
+return 0;
+
+}
+
